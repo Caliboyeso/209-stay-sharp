a44c06a4b9c4ab9c9f7f2e4817555df179fc8530
Jesus Lopez <jesus.blopez97@gmail.com>
2021-08-11 19:28:24 -0700

    testing media query.

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,7 +5,7 @@ import "./css/mediaQuery.css";
 function Footer() {
 
     return (
-        <div className="Footer-container">
+        <div className="Footer-container" id="Footer-container">
             <style>@import url('https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap');</style>
             <footer>
             <h3 id="Footer-title">209 Stay Sharp Barber Shop 2021&copy;</h3>
--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -19,7 +19,7 @@ function Gallery() {
       <style>@import url('https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap');</style>
       <h1 id="Gallery-title">Gallery</h1>
 
-      <Carousel fade id="Carousel">
+      <Carousel fade id="Carousel" className="Carousel" style={{width: "405px"}}>
         {/* Haircut #1 */}
         <Carousel.Item>
           <img
